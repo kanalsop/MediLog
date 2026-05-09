3b6a8dc1a2c9061c7f4f4bafcf344b69c3eeaea9
kanalsop <207140925+kanalsop@users.noreply.github.com>
2026-05-09 17:02:50 +0900

    chore: Init xcode project

---
+++ MediLog/MediLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediLogApp.swift
+//  MediLog
+//
+//  Created by Kanata Hirata on 2026/05/09.
+//
+
+import SwiftUI
+
+@main
+struct MediLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
